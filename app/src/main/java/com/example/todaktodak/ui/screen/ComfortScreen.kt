3e402be4a9bf66c6b5403160bc93ab316bdbb9de
Seokju Hong <iamseokjuhong@gmail.com>
2026-01-15 16:13:08 +0900

    ComfortScree Preview 추가

--- app/src/main/java/com/example/todaktodak/ui/screen/ComfortScreen.kt
+++ app/src/main/java/com/example/todaktodak/ui/screen/ComfortScreen.kt
@@ -2,10 +2,12 @@ package com.example.todaktodak.ui.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.todaktodak.ui.viewmodel.ComfortViewModel
@@ -53,4 +56,11 @@ fun ComfortScreen(
             )
         }
     }
+}
+
+
+@Preview
+@Composable
+fun ComfortScreePreview() {
+    ComfortScreen()
 }
